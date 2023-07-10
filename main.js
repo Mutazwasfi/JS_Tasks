cbeba929b462e7908031052919b31e8222d31d04
Mutazwasfi <Mutazwasfi5@gmail.com>
2023-07-10 17:01:52 +0300

    Done Day 2

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-function func1(){
-    var para = "hello world";
-    console.log(para)
-}
+// 'use strict'
 
-function func2(){
-    let para1 = 'code 1';
-    console.log(para1)
-}
-console.log(para,"para result");
+// let a = 10;
+// // const b = 10
 
-func1()
-func2()
+// a = 11
+// // b = 12
 
-let i=8
+// if (a <= 10){
+//     console.log('valid')
+// }
+
+//is it strict ?
+
+// let c = '5'
+// let d = 5
+// switch(Number(c)){
+//     case 1:
+//         console.log('this aint gonna be accessed');
+//         break;
+//     case 2:
+//         console.log('this aint gonna be accessed');
+//         break;
+//     case 3:
+//         console.log('this aint gonna be accessed');
+//         break;
+//     case 4:
+//         console.log('this aint gonna be accessed');
+//         break;
+//     case 5:
+//         console.log('will this should be...');
+//         break;
+//     default:
+//         console.log('here its on u')
+// }
+
+// //  variable deceleration ; condition statement ; whats done after each itteration  
+for(let i = 0 ; i < 10 ; i++)
+    console.log(`i equals:${i}`)
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10,11]
+
+for (let i = 0 ; i < arr.length ; i++)
+    console.log(arr[i])
+
+`this is a template literal.
+ ${expression} it act as a paragraph
+  string. we can also call functions`
